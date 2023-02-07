ff263d8829b8c2cd10218b25105fe002a7811cc3
Tamara Savadyan <94462838+TamaraSavadyan@users.noreply.github.com>
2023-02-07 21:55:04 +0300

    Update kv_vk.py

--- tasks/kv_vk.py
+++ tasks/kv_vk.py
@@ -23,7 +23,6 @@ def flip_kv_vk(d: dict[KT, KV]) -> dict[KV, KT]:
         'Москва': 'moscow',
     }
     """
-    raise NotImplementedError
     
     new_d = dict()
     for key, value in d.items():
@@ -45,7 +44,6 @@ def flip_kv_vk_safe(d: dict[KT, KV]) -> dict[KV, list[KT]]:
         '+3': ['Москва', 'Санкт-Петербург'],
     }
     """
-    raise NotImplementedError
     
     new_d = dict()
     for key, value in d.items():
