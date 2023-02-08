5aea9bf6fe238286bd6b24f938a8efd8f71f15cf
TamaraSavadyan <savadyan.tamara@gmail.com>
2023-02-08 21:38:39 +0300

    changed text.py

--- tasks/text.py
+++ tasks/text.py
@@ -11,7 +11,7 @@ def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]
     В переданном тексте вернуть слово имеющее наименьшую и наибольшую длину.
     Если такого слова нет - вернуть None
     """
-    new = re.split('\W+', text.strip())
+    new = re.split('\\W+', text.strip())
     new.sort(key=len)
     if new[0]:
         return (new[0], new[-1])
